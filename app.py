71783e18d0d09406e7676c1e6b343cc1038b8b9a
Suraj Bhatt <40247551+suribhatt@users.noreply.github.com>
2021-01-15 15:22:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+# importing the necessary dependencies
+from flask import Flask, render_template, request, jsonify
+import sklearn
+import pickle
+import numpy as np
+import joblib
+import pandas as pd
+app = Flask(__name__)  # initializing a flask app
+
+
+@app.route('/')  # route to display the home page
+def homePage():
+    return render_template("index.html")
+
+
+
+@app.route('/predict', methods=['POST'])  # route to show the predictions in a web UI
+def index():
+    # # reading the inputs given by the user
+    Triceps_skinfold = (request.form['Triceps_skinfold'])
+    Diabetes_fun = (request.form['Diabetes_function'])
+    pregnant = (request.form['pregnant'])
+    Plasma_glucose = (request.form['Plasma_glucose'])
+    Diastolic_bp = (request.form['Diastolic_bp'])
+    Hour_serum = (request.form['Hour_serum'])
+    bmi = (request.form['bmi'])
+    Age = (request.form['Age'])
+
+    co = ['Number of times pregnant', 'Plasma glucose concentration',
+          'Diastolic blood pressure (mm Hg)', 'Triceps skinfold thickness (mm)',
+          '2-Hour serum insulin (mu U/ml)',
+          'Body mass index (weight in kg/(height in m)^2)',
+          'Diabetes pedigree function', 'Age']
+
+    int_features = [x for x in request.form.values()]
+
+    final_features = np.array([[Triceps_skinfold,Diabetes_fun,pregnant,Plasma_glucose,Diastolic_bp,Hour_serum,bmi,Age]])
+
+    filename = "xgboost_model_suraj.pickle"
+    gg= "scaler_model.pickle"
+    scaler_model = pickle.load(open(gg, 'rb'))
+    d = scaler_model.transform(final_features)
+    loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
+    # predictions using the loaded model file
+    prediction = loaded_model.predict(d)
+
+    print('prediction is', prediction[0])
+    # showing the prediction results in a UI
+    return render_template('results.html', prediction=prediction[0])
+
+
+# @app.route('/postman', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+# def postm():
+#
+#     print(request.get_json(force=True))
+#     data= request.get_json(force=True)
+#     age = (data['age'])
+#     parch = (data['parch'])
+#     pclass = (data['pclass'])
+#     sibsp = (data['sibsp'])
+#     fare = (data['fare'])
+#     gender = (data['gender'])
+#     q = (data['q'])
+#     s = (data['s'])
+#
+#     filename = "titanic.sav"
+#     loaded_model = joblib.load(open(filename, 'rb'))  # loading the model file from the storage
+#     # predictions using the loaded model file
+#     prediction = loaded_model.predict([[pclass, age, sibsp, parch, fare, gender, q, s]])
+#     print('prediction is', prediction)
+#     prediction = round(100 * prediction[0])
+#     # showing the prediction results in a UI
+#     return jsonify({'Prediction': prediction})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # running the app
